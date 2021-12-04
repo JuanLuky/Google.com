3aba789f63115a35e0cddd55bc272a4779ebdea8
Juan Carlos <86580442+JuanLuky@users.noreply.github.com>
2021-12-04 10:29:24 -0300

    up

--- js/app.js
+++ js/app.js
@@ -5,3 +5,15 @@ search = () => {
     let url ='https://www.google.com/search?q='+valueInput;
     window.open(url,'_self');
 }
+
+window.addEventListener('keydown', function(event) {
+    if(event.key == "Enter"){
+        let inputText = document.querySelector('.query');
+        let valueInput = inputText.value;
+        let url ='https://www.google.com/search?q='+valueInput;
+        window.open(url,'_self');
+    }
+});
+
+
+
